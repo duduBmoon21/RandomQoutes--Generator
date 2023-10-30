40c52896134a1416ab6c3dc8dca4a86223c67a7d
Aduye <NightBirdie@users.noreply.github.com>
2023-10-30 23:04:54 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+
+function getRandomQuote(quotes) {
+  return quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+export default function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [quote, setQuote] = useState(null);
+
+  useEffect(() => {
+    fetch("https://type.fit/api/quotes")
+      .then((res) => res.json())
+      .then((json) => {
+        setQuotes(json);
+        setQuote(json[0]);
+      });
+  }, []);
+
+  function getNewQuote() {
+    setQuote(getRandomQuote(quotes));
+  }
+
+  return (
+    <main>
+      <h1>Day 2 Quote Generator</h1>
+      <section className="card">
+       
+        <h3>
+          <span>“</span>
+          {quote?.text}
+        </h3>
+        <i>- {quote?.author}</i>
+        <button onClick={getNewQuote} className="btn">New Quote</button>
+      </section>
+    </main>
+  );
+}
